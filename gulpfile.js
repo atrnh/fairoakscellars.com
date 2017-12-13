def52134115b9f4182d80151f51a4792d5bbf07a
Ashley Trinh <ashqtrinh@gmail.com>
2017-12-12 16:25:57 -0800

    Migrated all current styles to bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,8 @@ const browserify = require('browserify');
 const source = require('vinyl-source-stream');
 const sass = require('gulp-sass');
 const postcss = require('gulp-postcss');
-const var sourcemaps = require('gulp-sourcemaps');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('autoprefixer');
 
 const config = {
     port: 5000,
@@ -50,7 +51,7 @@ gulp.task('js', () => {
 gulp.task('sass', () => {
     return gulp.src(config.paths.mainSass)
       .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest(`.`))
+      .pipe(gulp.dest('./src/css'))
       .pipe(connect.reload());
 });
 
@@ -59,7 +60,7 @@ gulp.task('css', () => {
     .pipe(sourcemaps.init())
     .pipe(postcss([require('autoprefixer')]))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(`${config.paths.dist}/css`))
+    .pipe(gulp.dest(`${config.paths.dist}`))
     .pipe(connect.reload());
 })
 
