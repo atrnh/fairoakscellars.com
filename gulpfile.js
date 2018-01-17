5304485d9014c106e02d4a92fe9474c33198c5ed
Ashley Trinh <ashqtrinh@gmail.com>
2018-01-17 12:21:22 -0800

    Minify HTML and JS

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,10 @@ const sass = require('gulp-sass');
 const postcss = require('gulp-postcss');
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('autoprefixer');
+const buffer = require('vinyl-buffer');
+const log = require('gulplog');
+const uglify = require('gulp-uglify');
+const htmlmin = require('gulp-htmlmin');
 
 const config = {
     port: 5000,
@@ -20,7 +24,8 @@ const config = {
           mainJs: './src/main.js',
           sass: './src/**/*.scss',
           css: './src/**/*.css',
-          mainSass: './src/main.scss'
+          mainSass: './src/main.scss',
+          html: './src/index.html'
         },
 };
 
@@ -38,19 +43,42 @@ gulp.task('open', ['connect'], () => {
       .pipe(open({ uri: `${config.devBaseUrl}:${config.port}/` }));
 });
 
+gulp.task('html', () => {
+  return gulp.src(config.paths.html)
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      removeComments: true,
+      useShortDoctype: true,
+      removeRedundantAttributes: true,
+      removeScriptTypeAttributes: true,
+      removeStyleLinkAttributes: true
+    }))
+    .pipe(gulp.dest('.'))
+    .pipe(connect.reload());
+});
+
 gulp.task('js', () => {
-    browserify(config.paths.mainJs)
-      .transform('babelify', { presets: ['env'] })
-      .bundle()
-      .on('error', console.error.bind(console))
+    const b = browserify({
+      entries: config.paths.mainJs,
+      debug: true,
+      transform: [['babelify', { presets: ['env'] }]]
+    });
+
+    return b.bundle()
       .pipe(source('bundle.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(uglify())
+        .on('error', log.error)
+      .pipe(sourcemaps.write('.'))
       .pipe(gulp.dest(`${config.paths.dist}/scripts`))
       .pipe(connect.reload());
 });
 
 gulp.task('sass', () => {
     return gulp.src(config.paths.mainSass)
-      .pipe(sass().on('error', sass.logError))
+      .pipe(sass())
+        .on('error', sass.logError)
       .pipe(gulp.dest('./src/css'))
       .pipe(connect.reload());
 });
@@ -70,6 +98,6 @@ gulp.task('watch', () => {
   gulp.watch(config.paths.css, ['css']);
 });
 
-gulp.task('default', ['open', 'watch', 'sass', 'css', 'js']);
+gulp.task('default', ['open', 'watch', 'sass', 'css', 'js', 'html']);
 
 gulp.task('build', ['sass', 'css', 'js']);
