787350fb2a3f89bd6af416be37762c93a3284491
Ashley Trinh <ashqtrinh@gmail.com>
2018-01-18 11:40:51 -0800

    Refactor for cleanliness & readability

--- src/main.js
+++ src/main.js
@@ -10,34 +10,24 @@ window.jQuery = $;
 const controller = new ScrollMagic.Controller();
 
 controller.scrollTo((newpos, f) => {
-  if (newpos === 0) {
-    TweenLite.to(
-      window,
-      0.5,
-      {
-        scrollTo: {
-          y: 0
-        }
-      }
+  const scrollToOptions = { y: newpos };
+
+  if (newpos !== 0) {
+    const mobileNavHeight = (
+      $('#mobile-nav').height() - $('#navCollapsedContent').height()
     );
-  } else {
-    const navHeight = $('nav').height();
-    TweenLite.to(
-      window,
-      0.5,
-      {
-        scrollTo: {
-          y: newpos,
-          offsetY: navHeight + 40
-        }
-    });
+    const navHeight = $('#destkop-nav').height() || mobileNavHeight;
+    scrollToOptions.offsetY = navHeight + 40;
   }
-  f();
+
+  TweenLite.to(window, 0.5, { scrollTo: scrollToOptions });
   $('#navCollapsedContent').collapse('hide');
+  f();  // callback
 });
 
 const updateUrl = (id) => {
   if (id === '#') { id = '/'; }
+
   if (window.history && window.history.pushState) {
     history.pushState('', document.title, id);
   }
@@ -45,22 +35,11 @@ const updateUrl = (id) => {
 
 $(document).on('click', 'a[href^=\\#]', function (e) {
   const id = $(this).attr('href');
-  console.log(`id ${id}`);
 
   if (id.length > 0) {
     e.preventDefault();
 
-    if (id === "#") {
-      controller.scrollTo(0, () => {
-        updateUrl(id);
-
-      });
-    } else {
-      console.log(id);
-      controller.scrollTo(id, () => {
-        updateUrl(id);
-
-      });
-    }
+    const newLocation = id === '#' ? 0 : id;
+    controller.scrollTo(newLocation, () => updateUrl(id));
   }
 });
